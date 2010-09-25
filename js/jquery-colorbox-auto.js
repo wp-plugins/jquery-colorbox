7e0239b2ab42e4a1403d81a5855935953e33dc67
techotronic <techotronic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-25 22:34:14 +0000

    rewrote jquery-colorbox-auto.js
    git-svn-id: https://plugins.svn.wordpress.org/jquery-colorbox/trunk@293568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery-colorbox-auto.js
+++ js/jquery-colorbox-auto.js
@@ -4,18 +4,14 @@
  *
  * @since 3.1
  * @author Arne Franken
+ * @author jrevillini
  *
  * adds colorbox-manual to ALL img tags that are found in the HTML output
  */
 jQuery(document).ready(function($) {
     $("img").each( function(index,obj){
-        $classValue = $(obj).attr("class");
-        if(!$classValue){
-            $(obj).attr("class","colorbox-manual");
-        }
-        if(!$classValue.match('colorbox')) {
-            $classValue += " colorbox-manual";
-            $(obj).attr("class",$classValue);
+        if(!$(obj).attr("class").match('colorbox')) {
+            $(obj).addClass('colorbox-manual');
         }
     });
 });
