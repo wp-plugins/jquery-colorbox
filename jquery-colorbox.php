5fd7899de6280b10e0d5010a8c66e2bd720e3e29
techotronic <techotronic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-21 10:33:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jquery-colorbox/tags/3.8.2@324932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-colorbox.php
+++ jquery-colorbox.php
@@ -6,7 +6,7 @@
  * Plugin Name: jQuery Colorbox
  * Plugin URI: http://www.techotronic.de/plugins/jquery-colorbox/
  * Description: Used to overlay images on the current page. Images in one post are grouped automatically.
- * Version: 3.8.1
+ * Version: 3.8.2
  * Author: Arne Franken
  * Author URI: http://www.techotronic.de/
  * License: GPL
@@ -19,7 +19,7 @@
 ?>
 <?php
 //define constants
-define('JQUERYCOLORBOX_VERSION', '3.8.1');
+define('JQUERYCOLORBOX_VERSION', '3.8.2');
 define('COLORBOXLIBRARY_VERSION', '1.3.15');
 
 if (!defined('JQUERYCOLORBOX_PLUGIN_BASENAME')) {
@@ -96,9 +96,9 @@ class jQueryColorbox {
         $defaultArray = jQueryColorbox::jQueryColorboxDefaultSettings();
         $this->colorboxSettings = wp_parse_args($usersettings, $defaultArray);
 
-        // Set default theme if no theme is set already
-        if (empty($this->colorboxThemes[$this->colorboxSettings['colorboxTheme']])) {
-            $this->colorboxSettings['colorboxTheme'] = $defaultArray['colorboxTheme'];
+        //only add link to meta box
+        if(isset($this->colorboxSettings['removeLinkFromMetaBox']) && !$this->colorboxSettings['removeLinkFromMetaBox']){
+            add_action('wp_meta',array(& $this, 'renderMetaLink'));
         }
 
         if(isset($this->colorboxSettings['autoColorbox']) && $this->colorboxSettings['autoColorbox']){
