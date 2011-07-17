5b5164676d221a47367973778ca37962bbc8a530
techotronic <techotronic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-17 12:39:14 +0000

    changing version to 4.2
    git-svn-id: https://plugins.svn.wordpress.org/jquery-colorbox/trunk@411017 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-colorbox.php
+++ jquery-colorbox.php
@@ -6,7 +6,7 @@
  * Plugin Name: jQuery Colorbox
  * Plugin URI: http://www.techotronic.de/plugins/jquery-colorbox/
  * Description: Used to overlay images on the current page. Images in one post are grouped automatically.
- * Version: 4.1
+ * Version: 4.2
  * Author: Arne Franken
  * Author URI: http://www.techotronic.de/
  * License: GPL
